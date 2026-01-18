63a5fa528f85c1638f918ff1d3e6bdb97b7b2c0d
Smirnov Anton <smir0181@gmail.com>
2026-01-18 14:49:05 +0500

    йц

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivy.core.window import Window
 from kivy.properties import NumericProperty
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.animation import Animation
+from playsound import playsound
 from kivy.core.audio import SoundLoader
 from kivy.clock import Clock
 Window.size = (340,550)
@@ -25,30 +26,31 @@ class SongCover(MDBoxLayout):
         self.anim.start(self)
 
     def play(self,widget):
+        #progress = Animation(value=1, d=34, t='linear')
         self.widget = widget
+        #self.progress.start(widget)
         self.rotate()
         self.sound=SoundLoader.load('Eva.mp3')
-        if self.sound:
-            self.sound.play()
-            self.progressEvent = Clock.schedule_interval(self.updateprogessbar, 1)
+        self.sound.play()
+        self.progressEvent = Clock.schedule_interval(self.updateprogessbar, 1)
+        #self.timeEvent = Clock.schedule_interval(self.settime, 1)
     def stop(self,widget):
         self.widget = widget
         #self.progress.start(widget)
         self.sound.stop()
 
     def updateprogessbar(self,wer):
-        if hasattr(self, 'sound') and self.sound and self.sound.length > 0:
-            self.progress=self.sound.get_pos()/self.sound.length
-            SongCover.we=self.progress
-            if SongCover.we>0.05:
-                self.rotate()
-            print(self.we)
+        self.progress=self.sound.get_pos()/self.sound.length
+        SongCover.we=self.progress
+        if SongCover.we>0.05:
+            self.rotate()
+        print(self.we)
 class MusicScreen(Screen):
     pass
 class MainApp(MDApp):
     def build(self):
-        #self.load_kv('main.kv')
-        self.song = 'D:/music'
+        self.load_kv('main.kv')
+        self.song = 'C:/Users/virvi/OneDrive/Документы/Python_progect/Git/music_player'
         #self.sdf = .0101
         playsc= os.listdir(self.song)
         Clock.schedule_interval(self.progg,1)
